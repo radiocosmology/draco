87d1d119276d2c425cb19dc336c277ffb2754877
Richard Shaw <jrs65@cita.utoronto.ca>
2014-09-04 17:56:54 -0400

    More documentation, and added `setup.py`

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'ch_pipeline',
+    version = 0.1,
+
+    packages = find_packages(),
+    author = "CHIME collaboration",
+    author_email = "jrs65@cita.utoronto.ca",
+    description = "CHIME Pipeline",
+    url = "http://bitbucket.org/chime/ch_pipeline/"
+)
