951854ebed8a466d3c12c710becb9e31c021b3d1
Richard Shaw <jrs65@cita.utoronto.ca>
2014-09-24 15:56:56 -0400

    Added a simple task that reads in sets of timestream files.

--- ch_pipeline/io.py
+++ ch_pipeline/io.py
@@ -21,6 +21,8 @@ import os
 
 from caput import pipeline
 from caput import config
+from caput import mpiutil
+import containers
 
 
 def _files_from_spec(inst_name, timerange, archive_root=None):
@@ -114,6 +116,82 @@ class FilesFromDatasetSpec(pipeline.TaskBase):
         files = _files_from_spec(dset['instrument'], dset['timerange'], self.archive_root)
 
         return files
+
+
+def _list_of_filelists(files):
+    # Take in a list of lists/glob patterns of filenames
+    import glob
+
+    f2 = []
+
+    for filelist in files:
+
+        if isinstance(filelist, str):
+            filelist = glob.glob(filelist)
+        elif isinstance(filelist, list):
+            pass
+        else:
+            raise Exception('Must be list or glob pattern.')
+        f2.append(filelist)
+
+    return f2
+
+
+class LoadFiles(pipeline.TaskBase):
+    """Load data ifrom specified files.
+
+    Attributes
+    ----------
+    files : glob pattern
+        List of sets of files to take in. Can either be lists of actual files,
+        or glob patterns. Example: [ 'dir1/*.h5', ['dir2/a.h5', 'dir2/b.h5']].
+
+    Examples
+    --------
+    This can be configured from a pipeline file like this:
+
+    .. code-block:: yaml
+
+        pipeline :
+            tasks:
+            -   type:   ch_pipeline.io.LoadFiles
+                out:    ts
+                params:
+                    files:
+                        -   "dir1/*.h5"
+                        -
+                            -   "dir2/a.h5"
+                            -   "dir2/b.h5"
+
+    Each set is fed through the pipeline individually. That is, using the
+    above example, the first call to `next`, creates a timestream from all the
+    `*.h5` files in `dir1`, and then passes it on. The second, and final
+    `next` call returns a timestream from `dir1/a.h5` and `dir2/b.h5`.
+    """
+
+    files = config.Property(proptype=_list_of_filelists)
+
+    def next(self):
+        """Load in each set of files.
+
+        Returns
+        -------
+        ts : containers.TimeStream
+            The timestream of each set of files.
+        """
+
+        if len(self.files) == 0:
+            raise pipeline.PipelineStopIteration
+
+        files = self.files.pop(0)
+
+        if mpiutil.rank0:
+            print "Starting read of [%i files]" % len(files)
+
+        ts = containers.TimeStream.from_acq_files(sorted(files))  # Ensure file list if sorted
+        ts.attrs['tag'] = 'meh'
+
+        return ts
 
 
 class SaveOutput(pipeline.TaskBase):
