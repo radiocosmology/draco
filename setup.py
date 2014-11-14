b7d22d01540d9ae453a9ecc587589d54659fa79c
Richard Shaw <jrs65@cita.utoronto.ca>
2014-11-14 11:22:01 -0500

    Fixed up version of the regridding code.

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 
 import numpy as np
 
+# Cython module for fast regridding
+rg_ext = Extension(
+    "ch_pipeline._regrid_work",
+    ["ch_pipeline/_regrid_work.pyx"],
+    include_dirs = [np.get_include()],
+    extra_compile_args=['-fopenmp'],
+    extra_link_args=['-fopenmp'],
+)
 
 setup(
     name = 'ch_pipeline',
@@ -11,8 +19,7 @@ setup(
     packages = find_packages(),
     package_data = { "ch_pipeline" : [ "data/*" ] },
 
-    include_dirs = [np.get_include()],
-    ext_modules = cythonize('ch_pipeline/_regrid_work.pyx'),
+    ext_modules = cythonize([rg_ext]),
 
     author = "CHIME collaboration",
     author_email = "jrs65@cita.utoronto.ca",
