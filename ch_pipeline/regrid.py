- Fixed datatype bug.  - Made SiderealCalibration mode robust.  - Added weighting based on calibration.

ch_pipeline/regrid.py
@@ -64,7 +64,7 @@ def band_wiener(R, Ni, Si, y, bw):
 
     # Initialise arrays
     xh = np.zeros((k, m), dtype=y.dtype)
-    nw = np.zeros((k, m), dtype=y.dtype)
+    nw = np.zeros((k, m), dtype=Ni.dtype)
 
     # Generate dirty estimate
     w = np.dot(Ni * y, R.T)