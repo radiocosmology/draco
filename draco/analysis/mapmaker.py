5775a7b6c74d948a30cfb437177cd2c8ad96bc65
Niels Oppermann <niels@cita.utoronto.ca>
2017-03-27 22:24:38 -0400

    Added mapmaker class that takes into account several independently varying point sources.

--- draco/analysis/mapmaker.py
+++ draco/analysis/mapmaker.py
@@ -305,6 +305,365 @@ class WienerMapMaker(BaseMapMaker):
         return a
 
 
+class PointSourceWienerMapMaker(BaseMapMaker):
+    r"""Generate a Wiener filtered map assuming that the signal is a Gaussian
+    random field described by a power-law power spectum plus a handfull of
+    point sources with known positions.
+
+    Attributes
+    ----------
+    prior_amp : float
+        An amplitude prior to use for the map maker. In Kelvin.
+    prior_tilt : float
+        Power law index prior for the power spectrum.
+    spec_ind : float
+        Spectral index to assume for the brightness temperature of the diffuse        component, for rescaling the power spectrum.
+    kps_amp : float
+        Amplitude of the point-source covariance; in Kelvin^2.
+    RA : list
+        List of the right-ascensions of the point sources; in degrees.
+    dec : list
+        List of the declinations of the point sources; in degrees.
+    ps_amp_file : string
+        Name of the file to store the point-source amplitudes in.
+
+    Notes
+    -----
+
+    The point source amplitudes are allowed to vary independently. They all
+    have the same prior variance, equal across frequencies, whereas the
+    diffuse component has a power spectrum that scales with frequency.
+    
+    An estimate of the point-source amplitudes is saved to a numpy
+    binary (in brightness-temperature units) [TO DO: turn this into a proper
+    task output].
+    """
+
+    prior_amp = config.Property(proptype=float, default=1.0)
+    prior_tilt = config.Property(proptype=float, default=0.5)
+    spec_ind = config.Property(proptype=float, default=2.8)
+    kps_amp = config.Property(proptype=float, default=1.0)
+    RA = config.Property(proptype=list)
+    dec = config.Property(proptype=list)
+    ps_amp_file = config.Property(proptype=str, default='ps_amps.npy')
+
+    def process(self, mmodes):
+        """Overrides the BaseMapMaker routine to allow for input of point source map.
+
+        Parameters
+        ----------
+        mmodes : containers.MModes
+
+        ps_map : containers.Map 
+        
+        Returns
+        -------
+        map : containers.Map
+        """
+
+        from cora.util import hputil
+        from caput import mpiutil
+        from ch_util import ephemeris as eph
+
+        # Fetch various properties
+        bt = self.beamtransfer
+        tel = bt.telescope
+        lmax = bt.telescope.lmax
+        mmax = min(bt.telescope.mmax, len(mmodes.index_map['m']) - 1)
+        nfreq = len(mmodes.index_map['freq'])
+        npol = bt.telescope.num_pol_sky
+        nps = len(self.RA)
+
+        #Save as class variables
+        self.tel = tel
+        self.lmax = lmax
+        self.mmax = mmax
+        self.nfreq = nfreq
+        self.npol = npol
+
+        def find_key(key_list, key):
+            try:
+                return map(tuple, list(key_list)).index(tuple(key))
+            except TypeError:
+                return list(key_list).index(key)
+            except ValueError:
+                return None
+
+        # Figure out mapping between the frequencies
+        bt_freq = self.beamtransfer.telescope.frequencies
+        mm_freq = mmodes.index_map['freq']['centre']
+
+        freq_ind = [ find_key(bt_freq, mf) for mf in mm_freq]
+
+        # Trim off excess m-modes
+        mmodes.redistribute('freq')
+        m_array = mmodes.vis[:(mmax + 1)]
+        m_array = m_array.redistribute(axis=0)
+
+        m_weight = mmodes.weight[:(mmax + 1)]
+        m_weight = m_weight.redistribute(axis=0)
+
+        # Create array to store final alms in.
+        alm = mpiarray.MPIArray((nfreq, 4, lmax + 1, mmax + 1), axis=3,
+                                dtype=np.complex128, comm=mmodes.comm)
+        alm[:] = 0.0
+
+        # Create array to store projections onto point-source positions in.
+        vis_pss = np.zeros((nfreq, nps), dtype = np.complex128)
+
+        # Create array to store matrix in point-source space in.
+        M_pss = np.zeros((nfreq, nps, nps), dtype = np.complex128)
+
+        # Create array to store the point source amplitudes.
+        ps_amps = np.zeros((nfreq, nps), dtype = np.float64)
+
+        #Load in the alms of the point sources.
+        self._get_ps_alms()
+
+        # Loop over all m's collecting the required components.
+        for mi, m in m_array.enumerate(axis=0):
+
+            for fi in range(nfreq):
+                v = m_array[mi, :, fi].view(np.ndarray)
+                a = alm[fi, ..., mi].view(np.ndarray)
+                Ni = m_weight[mi, :, fi].view(np.ndarray)
+
+                if mpiutil.rank0:
+                    print eph.datetime.now(), "DEBUG: Inside loop getting diagonal piece: fi, mi = ", fi, mi
+
+                #Get the diagonal (in m) piece of the inverse
+                a[:], Ainv, ps_amps_i = self._solve_m_diagonal(m, mi, fi, bt_freq[fi], v, Ni)
+#                a[:], Ainv = self._solve_m_diagonal(m, mi, fi, bt_freq[fi], v, Ni)
+
+                if mpiutil.rank0:
+                    print eph.datetime.now(), "DEBUG: Inside loop getting correction: fi, mi = ", fi, mi
+
+                #Get the ingredients for the correction in point-source space
+                M_pss_i, vis_pss_i = self._solve_m_correction(Ainv, m, mi, fi, v, Ni)
+
+                vis_pss[fi] = vis_pss[fi] + vis_pss_i
+                M_pss[fi] = M_pss[fi] + M_pss_i
+                ps_amps[fi] = ps_amps[fi] + ps_amps_i
+
+        if mpiutil.rank0:
+            print eph.datetime.now(), "DEBUG: Before redistribute."
+
+        for fi in range(nfreq):
+            #invert the point-source space covariance
+            M_pss[fi] = np.linalg.inv(np.identity(nps)/self.kps_amp + M_pss[fi])
+            #multiply the point-source-projected visibilities with this matrix
+            vis_pss[fi] = np.dot(M_pss[fi], vis_pss[fi])
+
+        for mi, m in m_array.enumerate(axis=0):
+
+            for fi in range(nfreq):
+                a = alm[fi, ..., mi].view(np.ndarray)
+                Ni = m_weight[mi, :, fi].view(np.ndarray)
+
+                if mpiutil.rank0:
+                    print eph.datetime.now(), "DEBUG: Assembling results: fi, mi = ", fi, mi
+
+                corr = self._spread_ps_results(vis_pss[fi], m, mi, fi, Ni)
+
+                acorr, Ainv, pscorr = self._solve_m_diagonal(m, mi, fi, bt_freq[fi], corr, Ni, prewhiten=False)
+#                acorr, Ainv = self._solve_m_diagonal(m, mi, fi, bt_freq[fi], corr, Ni, prewhiten=False)
+                a[:] = a[:] - acorr
+                ps_amps[fi] = ps_amps[fi] - pscorr
+
+        alm = alm.redistribute(axis=0)
+
+        # Copy into square alm array for transform
+        almt = mpiarray.MPIArray((nfreq, 4, lmax + 1, lmax + 1), dtype=np.complex128, axis=0, comm=mmodes.comm)
+        almt[..., :(mmax + 1)] = alm
+        alm = almt
+
+        # Perform spherical harmonic transform to map space
+        maps = hputil.sphtrans_inv_sky(alm, self.nside)
+        maps = mpiarray.MPIArray.wrap(maps, axis=0)
+
+        m = containers.Map(nside=self.nside, axes_from=mmodes, comm=mmodes.comm)
+        m.map[:] = maps
+
+        np.save(self.ps_amp_file,ps_amps)
+
+        return m
+
+
+    def _solve_m_diagonal(self, m, mi, f, freq, v, Ni, prewhiten=True):
+        #perform the regular Wiener inversion (i.e., the part that's diagonal
+        #in m)
+
+        import scipy.linalg as la
+
+        bt = self.beamtransfer
+        lmax = bt.telescope.lmax
+        nps = len(self.RA)
+
+        # Massage the arrays into shape
+        v = v.reshape(bt.ntel)
+        Ni = Ni.reshape(bt.ntel)
+        Nh = Ni**0.5
+
+        # Get the beam transfer matrix, but trim off any l < m.
+        bm = bt.beam_m(m, fi=f)[..., m:].copy()
+        bm[np.isnan(bm)] = 0.
+
+        bm = bm.reshape(bt.ntel, -1)
+
+        # Construct pre-wightened beam and beam-conjugated matrices
+        bmt = bm * Nh[:, np.newaxis]
+        bth = bmt.T.conj()
+
+        # Pre-wighten the visibilities
+        if prewhiten:
+            vt = Nh * v
+        else:
+            vt = v
+
+        # Construct the signal covariance matrix
+        l = np.arange(lmax + 1)
+        l[0] = 1  # Change l=0 to get around singularity
+        l = l[m:]  # Trim off any l < m
+        cl_TT = self.prior_amp * l**(-self.prior_tilt)
+        #correct amplitude for spectral index
+        cl_TT *= (bt.telescope.frequencies[f]/408.)**(-2.*self.spec_ind)
+        S_diag = np.concatenate([cl_TT] * 4)
+
+        # Rearrange for blockwise matrix inversion
+        pCi = np.identity(bt.ntel) + np.dot(bmt * S_diag[np.newaxis, :], bth)
+        pC = la.inv(pCi)
+        vis_wiener = np.dot(pC, vt)
+        a_wiener = np.dot(bth, vis_wiener)
+
+        #Get the Y_l's for this m, but trim any l<m.
+        Yl = np.array([self.col_alm[mi, ii, f][..., m:].ravel() for ii in range(nps)])
+        ps_amps = self.kps_amp*np.array([np.dot(Yl[ii].conjugate(),a_wiener) for ii in range(nps)])
+
+        a_wiener = S_diag * a_wiener
+
+        # Copy the solution into a correctly shaped array output
+        a = np.zeros((bt.telescope.num_pol_sky, lmax + 1), dtype=v.dtype)
+        a[:, m:] = a_wiener.reshape(bt.telescope.num_pol_sky, -1)
+
+        return a, pC, ps_amps
+
+
+    def _solve_m_correction(self, Ainv, m, mi, f, v, Ni):
+        #Get the ingredients for the correction in point-source space
+
+        import scipy.linalg as la
+
+        bt = self.beamtransfer
+        lmax = bt.telescope.lmax
+        nps = len(self.RA)
+
+        # Massage the arrays into shape
+        v = v.reshape(bt.ntel)
+        Ni = Ni.reshape(bt.ntel)
+        Nh = Ni**0.5
+
+        # Get the beam transfer matrix, but trim off any l < m.
+        bm = bt.beam_m(m, fi=f)[..., m:].copy()
+        bm[np.isnan(bm)] = 0.
+
+        bm = bm.reshape(bt.ntel, -1)
+
+        # Construct pre-wightened beam and beam-conjugated matrices
+        bmt = bm * Nh[:, np.newaxis]
+        bth = bmt.T.conj()
+
+        # Pre-wighten the visibilities
+        vt = Nh * v
+        vt = np.dot(Ainv, vt)
+
+        #Get the Y_l's for this m, but trim any l<m.
+        Yl = np.array([self.col_alm[mi, ii, f][..., m:].ravel() for ii in range(nps)])
+
+        #Compute A^{-1}U
+        Bu = np.array([np.dot(bmt, Yl[ii]) for ii in range(nps)])
+        ui = np.array([np.dot(Ainv, Bu[ii]) for ii in range(nps)])
+
+        #Compute the piece of the point-source covariance for this m.
+        M_pss_i = np.array([[np.dot(Bu[ii].T.conj(), ui[jj]) for jj in range(nps)] for ii in range(nps)])
+
+        #Compute the point-source-projected visibilities for this m.
+        vis_pss_i = np.array([np.dot(Bu[ii].T.conj(), vt) for ii in range(nps)])
+
+        return M_pss_i, vis_pss_i
+
+
+    def _spread_ps_results(self, vis_pss, m, mi, f, Ni):
+        #project the results of the point-source correction back into map space
+
+        bt = self.beamtransfer
+        lmax = bt.telescope.lmax
+        nps = len(self.RA)
+
+        # Massage the arrays into shape
+        Ni = Ni.reshape(bt.ntel)
+        Nh = Ni**0.5
+
+        # Get the beam transfer matrix, but trim off any l < m.
+        bm = bt.beam_m(m, fi=f)[..., m:].copy()
+        bm[np.isnan(bm)] = 0.
+
+        bm = bm.reshape(bt.ntel, -1)
+
+        # Construct pre-wightened beam and beam-conjugated matrices
+        bmt = bm * Nh[:, np.newaxis]
+        bth = bmt.T.conj()
+
+        Yl = np.array([self.col_alm[mi, ii, f][..., m:].ravel() for ii in range(nps)])
+        Bu = np.array([np.dot(bmt, Yl[ii]) for ii in range(nps)])
+        corr = np.zeros(Bu[0].shape, dtype=complex)
+        for ii in range(nps):
+            corr += vis_pss[ii]*Bu[ii] 
+
+        return corr
+
+
+    def _get_ps_alms(self):
+        #Calculate the alms for each point source. At the moment this is done in
+        #a stupid way by creating a map with one non-zero pixel each and
+        #calculating the spherical-harmonic transform of those maps.
+        #TO DO: replace this procedure with a direct evaluation of spherical
+        #harmonics, using a fast and accurate library (perhaps pyGSL or pyshtools)
+        
+        from caput import mpiutil
+        from cora.util import hputil
+
+        nfreq = self.nfreq
+        npol = self.npol
+        mmax = self.mmax
+        lmax = self.lmax
+        tel = self.beamtransfer.telescope
+        nps = len(self.RA)
+
+        row_maps = [containers.Map(nside=self.nside,freq=nfreq) for ii in range(nps)]
+        for ii in range(nps):
+            row_maps[ii].map[:] = 0.
+            theta = np.pi / 2. - self.dec[ii] / 180. * np.pi
+            phi = self.RA[ii] / 180. * np.pi
+            pix = hputil.healpy.ang2pix(self.nside,theta,phi)
+            row_maps[ii].map[:,0,pix] = 1.
+
+        # Calculate the alms of input point source map
+        row_alm = np.array([hputil.sphtrans_sky(row_maps[ii].map[:], lmax=lmax).reshape((-1, npol * (lmax + 1), lmax + 1)) for ii in range(nps)])
+
+        # Trim off excess m's and wrap into MPIArray
+        row_alm = row_alm[..., :(mmax+1)]
+        row_alm = mpiarray.MPIArray.wrap(row_alm, axis=1)
+
+        # Perform the transposition to distribute different m's across processes.
+        col_alm = row_alm.redistribute(axis=3)
+
+        # Transpose and reshape to shift m index first.
+        col_alm = col_alm.transpose((3, 0, 1, 2)).reshape((None, nps, nfreq, npol, lmax+1))
+
+        self.col_alm = col_alm
+
+
+
 def pinv_svd(M, acond=1e-4, rcond=1e-3):
     # Generate the pseudo-inverse from an svd
     # Not really clear why I'm not just using la.pinv2 instead,
