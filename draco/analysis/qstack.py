83c16a436d1ef01fdbcd25190e9458148ebc1528
Mateus Arantes Fandino <fandinomat@gmail.com>
2019-04-07 19:08:25 -0700

    Add task QuasarStackFromMaps

--- draco/analysis/qstack.py
+++ draco/analysis/qstack.py
@@ -1,6 +1,7 @@
 import h5py
 import numpy as np
-from caput import mpiarray, config, mpiutil
+import healpy as hp
+from caput import mpiarray, config, mpiutil, pipeline
 from ..core import task, containers, io
 from cora.util import units
 from ch_util import tools, andata, ephemeris
@@ -63,18 +64,14 @@ class QuasarStack(task.SingleTask):
     # In other words: will we receive a sideral stream a single time
     # or multiple times to add up in the stack?
     def process(self, data):
-        """Smooth the weights with a median filter.
+        """
 
         Parameters
         ----------
-        data : :class:`andata.CorrData` or :class:`containers.TimeStream`
-            Data containing the weights to be smoothed
+        data : :class:`containers.SiderealStream`
 
         Returns
         -------
-        data : Same object as data
-            Data object containing the same data as the input, but with the
-            weights substituted by the smoothed ones.
         """
         # Ensure data is distributed in vis-stack axis
         data.redistribute(1)
@@ -227,3 +224,130 @@ class QuasarStack(task.SingleTask):
             result = result and is_copol
 
         return result
+
+
+class QuasarStackFromMaps(task.SingleTask):
+    """Blah.
+
+    Attributes
+    ----------
+    qcat_path : str
+            Full path to quasar catalog to stack on.
+    map_path : str
+            Full path to map to stack on.
+
+
+    """
+    # TODO: Change the map from a config parameter to
+    # an object loaded with another task and passed as an 
+    # argument to setup.
+
+    # Full path to quasar catalog to stack on
+    qcat_path = config.Property(proptype=str)
+    map_path = config.Property(proptype=str)
+
+    # Number of frequencies to keep on each side of quasar RA
+    # Pick only frequencies around the quasar (50 on each side)
+    freqside = config.Property(proptype=int, default=50)
+
+    def setup(self):
+        """Load quasar catalog and initialize the stack array.
+
+        """
+        # Load base quasar catalog from file (Not distributed)
+        self._qcat = containers.SpectroscopicCatalog.from_file(
+                                                    self.qcat_path)
+        self.nqso = len(self._qcat['position'])
+
+        # Size of quasar stack array
+        self.nstack = 2 * self.freqside + 1
+
+        # Quasar stack array.
+        self.quasar_stack = mpiarray.MPIArray.wrap(
+                np.zeros(self.nstack, dtype='complex64'), axis=0)
+        # Keep track of number of quasars added to each frequency bin
+        # in the quasar stack array
+        self.quasar_weight = mpiarray.MPIArray.wrap(
+                np.zeros(self.nstack, dtype='complex64'), axis=0)
+
+        # Load map
+        self._map = h5py.File(self.map_path, 'r')
+
+    # Blah
+    def process(self):
+        """
+        """
+        # Blah
+        self.nside = hp.npix2nside(self._map['map'].shape[2])
+        nfreq = len(self._map['index_map']['freq'])
+
+        # Find which quasars are in the frequency range of the data.
+        # Frequency of quasars
+        qso_freq = NU21/(self._qcat['redshift']['z'] + 1.)  # MHz.
+        # Sidereal stream frequency bin edges
+        freqbins = (self._map['index_map']['freq']['centre'] +
+                    0.5 * self._map['index_map']['freq']['width'])
+        freqbins = np.append(freqbins, self._map['index_map']['freq']['centre'][-1] -
+                             0.5 * self._map['index_map']['freq']['width'][-1])
+        # Frequency index of quasars (-1 due to np.digitize behaviour)
+        qso_findex = np.digitize(qso_freq, freqbins) - 1
+        # Only quasars in the frequency range of the data.
+        qso_selection = np.where((qso_findex >= 0) & (qso_findex < nfreq))[0]
+
+        # Indices to distribute qso amongst ranks
+        local_size, local_offset, _ = mpiutil.split_local(len(qso_selection))
+
+        # For each quasar assigned to this rank
+        for ii in range(local_size):
+
+            qq = ii + local_offset
+            ra = self._qcat['position']['ra'][qq]
+            dec = self._qcat['position']['dec'][qq]
+            pix = self.radec2pix(ra, dec)
+
+            # Pick only frequencies around the quasar (50 on each side)
+            # Indices to be processed in full frequency axis
+            lowindex = np.amax((0, qso_findex[qq] - self.freqside))
+            upindex = np.amin((nfreq, qso_findex[qq] + self.freqside + 1))
+            f_slice = np.s_[lowindex:upindex]
+            # Corresponding indices in quasar stack array
+            lowindex = lowindex - qso_findex[qq] + self.freqside
+            upindex = upindex - qso_findex[qq] + self.freqside
+            qs_slice = np.s_[lowindex:upindex]
+
+            # Fringestop, apply weight and sum.
+            self.quasar_stack[qs_slice] += self._map['map'][f_slice][:, 0, pix]
+            # Increment wheight for the appropriate quasar stack indices.
+            self.quasar_weight[qs_slice] += 1.
+
+        # Gather quasar stack for all ranks. Each contains the sum
+        # over a different subset of quasars.
+        quasar_stack_full = np.zeros(mpiutil.size*self.nstack,
+                                     dtype=self.quasar_stack.dtype)
+        # Gather all ranks
+        mpiutil.world.Allgather(self.quasar_stack,
+                                quasar_stack_full)
+        # Construct frequency offset axis
+        freq_offset = self._map['index_map']['freq'][int(nfreq/2) - self.freqside:
+                                             int(nfreq/2) + self.freqside + 1]
+        freq_offset['centre'] = (freq_offset['centre'] -
+                                 freq_offset['centre'][self.freqside])
+        # Container to hold the stack
+        qstack = containers.FrequencyStack(freq=freq_offset)
+        # Sum across ranks and take real part to complete the FT
+        qstack.stack[:] = np.sum(quasar_stack_full.reshape(
+                                    mpiutil.size, self.nstack), axis=0).real
+        qstack.weight[:] = self.quasar_weight  # The same for all ranks.
+
+        # This is needed because there is no argument to proecess()
+        # Once I implement the map as an argument this will not be necessary any more.
+        raise pipeline.PipelineStopIteration()
+
+        return qstack
+
+    def pix2radec(self, index):
+        theta, phi = hp.pixelfunc.pix2ang(self.nside,index)
+        return np.degrees(pi*2.-phi), -np.degrees(theta-pi/2.)
+
+    def radec2pix(self, ra, dec):
+        return hp.pixelfunc.ang2pix(self.nside, np.radians(-dec+90.), np.radians(360.-ra))
