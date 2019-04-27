00bcaeef2e893111af66e9f9db22f3c1e1f89f9b
Mateus Arantes Fandino <fandinomat@gmail.com>
2019-04-27 15:07:48 -0700

    Modified stacking sum method to use bincounts

--- draco/analysis/qstack.py
+++ draco/analysis/qstack.py
@@ -50,14 +50,30 @@ class QuasarStack(task.SingleTask):
 
         # Quasar stack array.
         self.quasar_stack = mpiarray.MPIArray.wrap(
-                np.zeros(self.nstack, dtype='complex64'), axis=0)
+                np.zeros(self.nstack, dtype=np.float), axis=0)
         # Keep track of number of quasars added to each frequency bin
         # in the quasar stack array
         self.quasar_weight = mpiarray.MPIArray.wrap(
-                np.zeros(self.nstack, dtype='complex64'), axis=0)
+                np.zeros(self.nstack, dtype=np.float), axis=0)
 
         # Get the TransitTelescope object
         self.telescope = io.get_telescope(manager)
+
+        # TODO: New stuff for applying bincount method
+        # This is a hack, to have it work quickly with the sims
+        # and data. Should have the default be 50/0.39... and
+        # anything different should be passed as an array in the config?
+        self.stackside = int(self.nstack)//int(2)
+        self.stack_axis = np.zeros(self.nstack,
+                dtype=[('centre', np.float), ('width', np.float)])
+        if self.freqside == 50:
+            self.stack_axis['width'][:] = 0.390625
+            self.stack_axis['centre'][:] = np.linspace(50.*0.390625, -50.*0.390625, self.nstack, endpoint=True) 
+        else:
+            self.stack_axis['width'][:] = 1.5625
+            self.stack_axis['centre'][:] = np.linspace(12.*1.5625, -12.*1.5625, self.nstack, endpoint=True) 
+
+
 
     # TODO: Should data be an argument to process or init?
     # I think data should be an argument to process() such that we could
@@ -74,7 +90,7 @@ class QuasarStack(task.SingleTask):
         Returns
         -------
         """
-        nfreq = len(data.index_map['freq'])
+        nfreq = len(data.index_map['freq']['centre'])
 
         # RA bins each quasar is closest to. 
         # Phasing will actually be done at quasar position.
@@ -89,15 +105,19 @@ class QuasarStack(task.SingleTask):
         # Find which quasars are in the frequency range of the data.
         # Frequency of quasars
         qso_freq = NU21/(self._qcat['redshift']['z'] + 1.)  # MHz.
-        # Sidereal stream frequency bin edges
-        freqbins = (data.index_map['freq']['centre'] +
-                    0.5*data.index_map['freq']['width'])
-        freqbins = np.append(freqbins, data.index_map['freq']['centre'][-1] -
-                             0.5*data.index_map['freq']['width'][-1])
-        # Frequency index of quasars (-1 due to np.digitize behaviour)
-        qso_findex = np.digitize(qso_freq, freqbins) - 1
+        # Get f_mask and qs_indices
+        freqdiff = data.index_map['freq']['centre'][np.newaxis, :] - qso_freq[:, np.newaxis]
+        # Stack axis bin edges to digitize each quasar at.
+        stackbins = (self.stack_axis['centre'] +
+                    0.5 * self.stack_axis['width'])
+        stackbins = np.append(stackbins, self.stack_axis['centre'][-1] -
+                    0.5 * self.stack_axis['width'][-1])
+        # Index of each frequency in stack axis, for each quasar
+        qs_indices = np.digitize(freqdiff, stackbins) - 1
+        # Indices to be processed in full frequency axis for each quasar
+        f_mask = ((qs_indices>=0) & (qs_indices<self.nstack))
         # Only quasars in the frequency range of the data.
-        qso_selection = np.where((qso_findex >= 0) & (qso_findex < nfreq))[0]
+        qso_selection = np.where(np.sum(f_mask, axis=1)>0)[0]
 
         # Compute baselines and map visibilities polarization
         bvec_m, pol_array = self._bvec_m(data)
@@ -148,17 +168,7 @@ class QuasarStack(task.SingleTask):
                                         self._qcat['position']['ra'][qq],
                                         ha_side)
 
-            # Pick only frequencies around the quasar (50 on each side)
-            # Indices to be processed in full frequency axis
-            lowindex = np.amax((0, qso_findex[qq] - self.freqside))
-            upindex = np.amin((nfreq, qso_findex[qq] + self.freqside + 1))
-            f_slice = np.s_[lowindex:upindex]
-            # Corresponding indices in quasar stack array
-            lowindex = lowindex - qso_findex[qq] + self.freqside
-            upindex = upindex - qso_findex[qq] + self.freqside
-            qs_slice = np.s_[lowindex:upindex]
-
-            nu = data.index_map['freq']['centre'][f_slice]
+            nu = data.index_map['freq']['centre'][f_mask[qq]]
             # For each polarization
             for pp in range(2):
                 # Baseline vectors in wavelengths. Shape:
@@ -188,16 +198,22 @@ class QuasarStack(task.SingleTask):
                 correc *= visweight
 
                 # Fringestop, apply weight and sum.
-                self.quasar_stack[qs_slice] += np.sum(
-                    np.sum(copol_vis[pp][f_slice][:, :, ra_index_range] *
-                           correc, axis=2), axis=1)
+                this_stack = np.sum(
+                    np.sum(copol_vis[pp][f_mask[qq]][:, :, ra_index_range] *
+                           correc, axis=2), axis=1).real
+                this_stack = np.bincount(qs_indices[qq][f_mask[qq]],
+                                         weights=this_stack,
+                                         minlength=self.nstack)
+                self.quasar_stack += this_stack
 
                 # Increment wheight for the appropriate quasar stack indices.
                 # The redundancy is the same for all frequencies and quasars,
                 # so it amounts to an overall multiplication factor in the weight
                 # that can be dropped. I add the beams to the weights because
                 # they differ from quasar to quasar and frequency to frequency.
-                self.quasar_weight[qs_slice] += np.sum(beam, axis=1)
+                self.quasar_weight += np.bincount(qs_indices[qq][f_mask[qq]],
+                                        weights=np.sum(beam, axis=1),
+                                        minlength=self.nstack)
 
         # TODO: In what follows I gather to all ranks and do the summing
         # in each rank. I end up with the same stack in all ranks, but there
@@ -223,11 +239,11 @@ class QuasarStack(task.SingleTask):
                                  freq_offset['centre'][self.freqside])
         # Container to hold the stack
         qstack = containers.FrequencyStack(freq=freq_offset)
-        # Sum across ranks and take real part to complete the FT
+        # Sum across ranks to complete the FT
         qstack.stack[:] = np.sum(quasar_stack_full.reshape(
-                                    mpiutil.size, self.nstack), axis=0).real
+                                 mpiutil.size, self.nstack), axis=0)
         qstack.weight[:] = np.sum(quasar_weight_full.reshape(
-                                    mpiutil.size, self.nstack), axis=0)
+                                  mpiutil.size, self.nstack), axis=0)
 
         print 'Number of quasars stacked: {0}'.format(qcount)
         return qstack
