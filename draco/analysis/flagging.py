fix(flagging): corrected calculation of TV channel flagging threshold

draco/analysis/flagging.py
@@ -527,12 +527,12 @@ def inverse_binom_cdf_prob(k, N, F):
     """Calculate the trial probability that gives the CDF.
 
     This gets the trial probability that gives an overall cumulative
-    probability for Pr(X < k; N, p) = F
+    probability for Pr(X <= k; N, p) = F
 
     Parameters
     ----------
     k : int
-        Number of successes.
+        Maximum number of successes.
     N : int
         Total number of trials.
     F : float
@@ -606,7 +606,11 @@ def tv_channels_flag(x, freq, sigma=5, f=0.5, debug=False):
         # Calculate the threshold to apply
         N = sel.sum()
         k = int(f * N)
-        t = p_to_sigma(inverse_binom_cdf_prob(k, N, p_false))
+
+        # This is the Gaussian threshold required for there to be at most a p_false chance of more
+        # than k trials exceeding the threshold. This is the correct expression, and has been double
+        # checked by numerical trials.
+        t = p_to_sigma(inverse_binom_cdf_prob(k, N, 1 - p_false))
 
         frac[sel] = (x[sel] > t).mean(axis=0)[np.newaxis, :]
 