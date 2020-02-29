c95b3b1d5857dc2db73cb01121fab14cb6f7d18b
Rick Nitsche <rick@phas.ubc.ca>
2020-02-28 16:05:19 -0800

    add test

--- test/test_selections.py
+++ test/test_selections.py
@@ -110,3 +110,58 @@ def test_H5FileSelect_distributed_on_disk(container_on_disk_distributed):
         assert np.all(md["weight"][:] == dset2[(fsel, slice(None))])
         assert np.all(md.index_map["freq"] == freqs[fsel])
         assert np.all(md.index_map["input"] == inputs[isel])
+
+
+def test_test_H5FileSelect_distributed_on_disk_simple():
+    """
+    Load distributed H5 into parallel container while down-selecting axes.
+
+    This test does the same as `test_H5FileSelect_distributed_on_disk` but it checks the
+    frequencies distributed to each node after selection instead of writing to disk
+    before checking.
+    """
+    if size != 4:
+        pytest.skip("This test has to be run with mpirun -np 4")
+    len_axis = 8
+
+    local_from = int(len_axis / size * rank)
+    local_to = int(len_axis / size * (rank + 1))
+    global_data1 = np.arange(len_axis * len_axis * len_axis, dtype=np.int)
+    local_data1 = global_data1.reshape(len_axis, -1, len_axis)[local_from:local_to]
+    d_array1 = mpiarray.MPIArray.wrap(local_data1, axis=0)
+    global_data2 = np.arange(len_axis * len_axis, dtype=np.int)
+    local_data2 = global_data2.reshape(len_axis, -1)[local_from:local_to]
+    d_array2 = mpiarray.MPIArray.wrap(local_data2, axis=0)
+
+    fname = "tmp_test_memh5_select_distributed_simple.h5"
+    container = SiderealGainData(freq=freqs, input=inputs, ra=ra)
+    container.create_dataset("gain", data=d_array1)
+    container.create_dataset("weight", data=d_array2)
+    container.save(fname)
+
+    # load file and apply selection
+    fsel = slice(5)
+    md = SiderealGainData.from_file(fname, freq_sel=fsel, distributed=True)
+
+    # test
+    if rank == 0:
+        # should hold freq indices 0 and 1
+        assert np.all(md["gain"][:] == dset1[(slice(2), slice(None), slice(None))])
+        assert np.all(md["weight"][:] == dset2[(slice(2), slice(None))])
+        assert np.all(md.index_map["freq"] == freqs[fsel])
+    else:
+        # should hold 1 freq index each
+        assert np.all(
+            md["weight"][:] == dset2[(slice(rank + 1, rank + 2), slice(None))]
+        )
+        assert np.all(
+            md["gain"][:]
+            == dset1[(slice(rank + 1, rank + 2), slice(None), slice(None))]
+        )
+        assert np.all(md.index_map["freq"] == freqs[fsel])
+
+    # tear down
+    file_names = glob.glob(fname + "*")
+    if rank == 0:
+        for fname in file_names:
+            os.remove(fname)
