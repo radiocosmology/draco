518db119d585a466289704057799611171d4575f
Richard Shaw <richard@phas.ubc.ca>
2020-04-28 19:14:06 -0700

    feat(ContainerBase): copy an existing container and share its data
    This adds a copy method to ContainerBase, that will allow specified
    datasets to be shared between different instances.

--- test/test_containers.py
+++ test/test_containers.py
@@ -12,6 +12,7 @@ def ss_container():
     )
     ss.attrs["test_attr1"] = "hello"
     ss.vis.attrs["test_attr2"] = "hello2"
+    ss.weight.attrs["test_attr3"] = "hello3"
 
     return ss
 
@@ -24,9 +25,50 @@ def test_attrs_from(ss_container):
     assert len(ts.attrs) == 1
     assert ts.attrs["test_attr1"] == "hello"
     assert ts.vis.attrs["test_attr2"] == "hello2"
+    assert ts.weight.attrs["test_attr3"] == "hello3"
 
-    # Check that the axis attributes on the dataasets did not get overwritten
+    # Check that the axis attributes on the datasets did not get overwritten
     assert len(ts.vis.attrs) == 2
-    assert len(ts.weight.attrs) == 1
+    assert len(ts.weight.attrs) == 2
     assert tuple(ts.vis.attrs["axis"]) == ("freq", "stack", "time")
     assert tuple(ts.weight.attrs["axis"]) == ("freq", "stack", "time")
+
+
+def test_copy(ss_container):
+
+    ss_container.vis[:] = np.arange(16)
+    ss_container.weight[:] = np.arange(16)
+
+    ss_copy = ss_container.copy(shared=("vis",))
+
+    # Check that the copy has worked
+    # Check the dataset values...
+    assert (ss_copy.vis[:] == ss_container.vis[:]).all()
+    assert (ss_copy.weight[:] == ss_container.weight[:]).all()
+    # Check the attributes...
+    assert ss_copy.attrs["test_attr1"] == "hello"
+    assert ss_copy.vis.attrs["test_attr2"] == "hello2"
+    assert ss_copy.weight.attrs["test_attr3"] == "hello3"
+
+    # Modify the datasets
+    ss_container.vis[:] = 1.0
+    ss_container.weight[:] = 2.0
+    ss_container.vis.attrs["test_attr4"] = "hello4"
+    ss_container.weight.attrs["test_attr5"] = "hello5"
+
+    # Check that we see the modified values
+    assert (ss_copy.vis[:] == 1.0).all()
+    assert (ss_copy.weight[:] == np.arange(16)).all()
+    assert ss_copy.vis.attrs["test_attr4"] == "hello4"
+    assert "test_attr5" not in ss_copy.weight.attrs
+
+    # These tests only make sense when running across multiple MPI processes
+    assert ss_container.vis.distributed_axis == 0
+    assert ss_copy.vis.distributed_axis == 0
+
+    current_shape = ss_copy.weight.local_shape
+
+    ss_container.redistribute("ra")
+
+    assert ss_copy.vis.local_shape == ss_container.vis.local_shape
+    assert ss_copy.weight.local_shape == current_shape
