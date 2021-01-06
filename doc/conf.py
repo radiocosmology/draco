eeaf20f65e6d4d099fea8bd12eac1c3a6b675ed1
Rick Nitsche <rick@phas.ubc.ca>
2021-01-06 14:50:17 -0800

    ci(docs): build docs without installing draco

--- doc/conf.py
+++ doc/conf.py
@@ -16,9 +16,10 @@ autodoc_mock_imports = ["mpi4py", "cora"]
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
