e1b3665f8bb97a29822659eef594c6844253acb3
Rick Nitsche <rick@phas.ubc.ca>
2021-01-06 15:24:23 -0800

    build without install

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,6 @@
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
-import draco
-
-autodoc_mock_imports = ["mpi4py", "cora"]
 
 # -- Path setup --------------------------------------------------------------
 
@@ -19,8 +16,11 @@ autodoc_mock_imports = ["mpi4py", "cora"]
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
 
+import draco
+
+autodoc_mock_imports = ["mpi4py", "cora", "driftscan", "caput"]
 
 # -- Project information -----------------------------------------------------
 
