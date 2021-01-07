d57d9d098d90422653f94c38b0f153a189fe1fcc
Rick Nitsche <rick@phas.ubc.ca>
2021-01-06 16:10:53 -0800

    fix

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("../"))
 
 import draco
 
-autodoc_mock_imports = ["mpi4py", "cora", "driftscan"]
+autodoc_mock_imports = ["mpi4py", "cora", "driftscan", "caput"]
 
 # -- Project information -----------------------------------------------------
 
