5bbeab4952f01f6e70c3a21612ac9be14b50fc7d
Seth Siegel <seth@Seths-Mac-mini.local>
2021-06-01 18:36:48 -0400

    feat(dayenu): capability to apply delay filter to ringmaps
    - Creates the DayenuDelayFilterMap task, which can apply the DAYENU
    delay filter to ring maps.
    
    - Allow arbitrary delay cutoffs when filtering maps by providing a
    file containing the delay cutoff as a function of el.
    
    - Switch to using micro-seconds rather than nano-seconds to specify
    the delay cutoff for consistency with delay.py.
    
    - Propagate weights through the delay filtering.
    
    - Improved memory usage in the case of a time-variable mask.
    
    - Add docstrings to all tasks/functions.

--- draco/analysis/dayenu.py
+++ draco/analysis/dayenu.py
@@ -1,26 +1,21 @@
-"""Delay space spectrum estimation and filtering.
-"""
-# === Start Python 2/3 compatibility
-from __future__ import absolute_import, division, print_function, unicode_literals
-from future.builtins import *  # noqa  pylint: disable=W0401, W0614
-from future.builtins.disabled import *  # noqa  pylint: disable=W0401, W0614
+"""DAYENU delay and m-mode filtering.
 
-# === End Python 2/3 compatibility
+See https://arxiv.org/abs/2004.11397 for a description.
+"""
 
 import time
 
 import numpy as np
 
-from caput import config
+from caput import config, memh5
 from cora.util import units
 
 from ..core import task, io
+from ..util import tools
 
 
 class DayenuDelayFilter(task.SingleTask):
-    """Apply DAYENU filter along the frequency axis.
-
-    See https://arxiv.org/abs/2004.11397.
+    """Apply a DAYENU high-pass delay filter to visibility data.
 
     Attributes
     ----------
@@ -36,19 +31,20 @@ class DayenuDelayFilter(task.SingleTask):
         baseline length. For cylindrical telescopes oriented in the
         NS direction (like CHIME) use 'NS'. The default is 'NS'.
     epsilon : float
-        Rejection in power.
+        The stop-band rejection of the filter.
     tauw : float
-        Delay cutoff in nanoseconds.
+        Delay cutoff in micro-seconds.
     single_mask : bool
-        Apply a single frequency mask for all times.  Only includes frequencies
-        where the weights are nonzero for all times.  Otherwise will construct
-        a filter for all unique single time frequency masks (much slower).
+        Apply a single frequency mask for all times.  Only includes
+        frequencies where the weights are nonzero for all times.
+        Otherwise will construct a filter for all unique single-time
+        frequency masks (can be significantly slower).
     """
 
     za_cut = config.Property(proptype=float, default=1.0)
     telescope_orientation = config.enum(["NS", "EW", "none"], default="NS")
     epsilon = config.Property(proptype=float, default=1e-10)
-    tauw = config.Property(proptype=float, default=100.0)
+    tauw = config.Property(proptype=float, default=0.100)
     single_mask = config.Property(proptype=bool, default=True)
 
     def setup(self, telescope):
@@ -60,19 +56,19 @@ class DayenuDelayFilter(task.SingleTask):
         """
         self.telescope = io.get_telescope(telescope)
 
-        self.log.info("Initial delay cut set to %0.1f nanosec." % self.tauw)
+        self.log.info("Instrumental delay cut set to %0.3f micro-sec." % self.tauw)
 
     def process(self, stream):
         """Filter out delays from a SiderealStream or TimeStream.
 
         Parameters
         ----------
-        stream : containers.SiderealStream
+        stream : SiderealStream
             Data to filter.
 
         Returns
         -------
-        stream_filt : containers.SiderealStream
+        stream_filt : SiderealStream
             Filtered dataset.
         """
         # Distribute over products
@@ -104,24 +100,31 @@ class DayenuDelayFilter(task.SingleTask):
 
             if self.single_mask:
                 flag = np.all(flag, axis=-1, keepdims=True)
-
-            weight[:, bb] *= flag.astype(weight.dtype)
+                weight[:, bb] *= flag.astype(weight.dtype)
 
             if not np.any(flag):
                 continue
 
+            var = tools.invert_no_zero(weight[:, bb])
+
             self.log.info(
-                "Filtering baseline %d of %d. [%0.1f nsec]" % (bb, nprod, bcut)
+                "Filtering baseline %d of %d. [%0.3f micro-sec]" % (bb, nprod, bcut)
             )
 
             # Construct the filter
-            NF = highpass_delay_filter(freq, bcut, flag, epsilon=self.epsilon)
+            NF, index = highpass_delay_filter(freq, bcut, flag, epsilon=self.epsilon)
 
             # Apply the filter
             if self.single_mask:
                 vis[:, bb] = np.matmul(NF[:, :, 0], vis[:, bb])
+                weight[:, bb] = tools.invert_no_zero(np.matmul(NF[:, :, 0] ** 2, var))
             else:
-                vis[:, bb] = np.sum(NF * vis[np.newaxis, :, bb], axis=1)
+                self.log.info("There are %d unique masks/filters." % len(index))
+                for ii, ind in enumerate(index):
+                    vis[:, bb, ind] = np.matmul(NF[:, :, ii], vis[:, bb, ind])
+                    weight[:, bb, ind] = tools.invert_no_zero(
+                        np.matmul(NF[:, :, ii] ** 2, var[:, ind])
+                    )
 
             self.log.info("Took %0.2f seconds." % (time.time() - t0,))
 
@@ -141,13 +144,161 @@ class DayenuDelayFilter(task.SingleTask):
         else:
             baselines = np.sqrt(np.sum(baselines ** 2, axis=-1))  # Norm
 
-        baseline_delay_cut = 1e9 * self.za_cut * baselines / units.c
+        baseline_delay_cut = 1e6 * self.za_cut * baselines / units.c
 
         return baseline_delay_cut + self.tauw
 
 
+class DayenuDelayFilterMap(task.SingleTask):
+    """Apply a DAYENU high-pass delay filter to ringmap data.
+
+    Attributes
+    ----------
+    epsilon : float
+        The stop-band rejection of the filter.
+    filename : str
+        The name of an hdf5 file containing the "delay_cutoff"
+        in micro-seconds as a function of "el" = sin(za).  If a
+        filename is provided, then its "delay_cutoff" dataset
+        will be  interpolated to determine the cutoff of the filter
+        based on the el coordinate of the map.  If a filename is not
+        provided, then a single cutoff given by the tauw property
+        will be used for all el.
+    tauw : float
+        Delay cutoff in micro-seconds.
+    single_mask : bool
+        Apply a single frequency mask for all times.  Only includes
+        frequencies where the weights are nonzero for all times.
+        Otherwise will construct a filter for all unique single-time
+        frequency masks (can be significantly slower).
+    """
+
+    epsilon = config.Property(proptype=float, default=1e-12)
+    filename = config.Property(proptype=str, default=None)
+    tauw = config.Property(proptype=float, default=0.100)
+    single_mask = config.Property(proptype=bool, default=True)
+
+    _ax_dist = "el"
+
+    def setup(self):
+        """Create the function used to determine the delay cutoff."""
+
+        if self.filename is not None:
+            self.log.info("Using delay cut from file: %s" % self.filename)
+
+            fcut = memh5.MemGroup.from_hdf5(self.filename)
+            kind = fcut.attrs.get("kind", "linear")
+
+            self._get_cut = scipy.interpolate.interp1d(
+                fcut.index_map["el"][:],
+                fcut["delay_cutoff"][:],
+                kind=kind,
+                bounds_error=False,
+                fill_value=self.tauw,
+            )
+
+        else:
+            self.log.info("Using constant delay cut of %0.3f micro-sec." % self.tauw)
+
+            self._get_cut = lambda el: np.full(el.size, self.tauw, dtype=np.float64)
+
+    def process(self, ringmap):
+        """Filter out delays from a RingMap.
+
+        Parameters
+        ----------
+        ringmap : RingMap
+            Data to filter.
+
+        Returns
+        -------
+        ringmap_filt : RingMap
+            Filtered data.
+        """
+        # Distribute over el
+        ringmap.redistribute(self._ax_dist)
+
+        # Extract the required axes
+        axes = list(ringmap.map.attrs["axis"])
+        ax_freq = axes.index("freq")
+        ax_dist = axes.index(self._ax_dist)
+
+        lshp = ringmap.map.local_shape[0:ax_freq]
+
+        freq = ringmap.freq[:]
+
+        nel = ringmap.map.local_shape[ax_dist]
+        sel = ringmap.map.local_offset[ax_dist]
+        eel = sel + nel
+
+        el = ringmap.index_map[self._ax_dist][sel:eel]
+
+        # Determine the baseline dependent cutoff
+        cutoff = self._get_cut(el)
+
+        # Dereference the required datasets
+        rm = ringmap.map[:].view(np.ndarray)
+        weight = ringmap.weight[:].view(np.ndarray)
+
+        # Loop over index
+        for ind in np.ndindex(*lshp):
+
+            wind = ind[1:]
+
+            for ee, ecut in enumerate(cutoff):
+
+                t0 = time.time()
+
+                slc = ind + (slice(None), slice(None), ee)
+                wslc = slc[1:]
+
+                # Flag frequencies and times with zero weight
+                flag = weight[wslc] > 0.0
+
+                if self.single_mask:
+                    flag = np.all(flag, axis=-1, keepdims=True)
+                    weight[wslc] *= flag.astype(weight.dtype)
+
+                if not np.any(flag):
+                    continue
+
+                self.log.info(
+                    "Filtering el %d of %d. [%0.3f micro-sec]" % (ee, nel, ecut)
+                )
+
+                erm = rm[slc]
+                evar = tools.invert_no_zero(weight[wslc])
+
+                # Construct the filter
+                NF, index = highpass_delay_filter(
+                    freq, ecut, flag, epsilon=self.epsilon
+                )
+
+                # Apply the filter
+                if self.single_mask:
+
+                    rm[slc] = np.matmul(NF[:, :, 0], erm)
+                    weight[wslc] = tools.invert_no_zero(
+                        np.matmul(NF[:, :, 0] ** 2, evar)
+                    )
+
+                else:
+
+                    self.log.info("There are %d unique masks/filters." % len(index))
+
+                    for ii, rr in enumerate(index):
+                        rm[ind][:, rr, ee] = np.matmul(NF[:, :, ii], erm[:, rr])
+                        weight[wind][:, rr, ee] = tools.invert_no_zero(
+                            np.matmul(NF[:, :, ii] ** 2, evar[:, rr])
+                        )
+
+                self.log.info("Took %0.2f seconds." % (time.time() - t0,))
+
+        return ringmap
+
+
 class DayenuMFilter(task.SingleTask):
-    """Apply Dayenu bandpass filter along the time axis.
+    """Apply a DAYENU bandpass m-mode filter.
 
     Attributes
     ----------
@@ -155,7 +306,7 @@ class DayenuMFilter(task.SingleTask):
         The bandpass filter is centered on the m corresponding to the
         fringe rate of a source at the meridian at this declination.
     epsilon : float
-        Rejection in power.
+        The stop-band rejection of the filter.
     fkeep_intra : float
         Width of the bandpass filter for intracylinder baselines in terms
         of the fraction of the telescope cylinder width.
@@ -165,7 +316,7 @@ class DayenuMFilter(task.SingleTask):
     """
 
     dec = config.Property(proptype=float, default=40.0)
-    epsilon = config.Property(proptype=float, default=1e-8)
+    epsilon = config.Property(proptype=float, default=1e-10)
     fkeep_intra = config.Property(proptype=float, default=0.75)
     fkeep_inter = config.Property(proptype=float, default=0.75)
 
@@ -183,12 +334,12 @@ class DayenuMFilter(task.SingleTask):
 
         Parameters
         ----------
-        stream : containers.SiderealStream
+        stream : SiderealStream
             Data to filter.
 
         Returns
         -------
-        stream_filt : containers.SiderealStream
+        stream_filt : SiderealStream
             Filtered dataset.
         """
         # Distribute over products
@@ -294,18 +445,40 @@ class DayenuMFilter(task.SingleTask):
         return m
 
 
-def highpass_delay_filter(freq, tau_cut, flag, epsilon=1e-8):
+def highpass_delay_filter(freq, tau_cut, flag, epsilon=1e-10):
+    """Construct a high-pass delay filter.
+
+    The stop band will range from [-tau_cut, tau_cut].
+
+    Parameters
+    ----------
+    freq : np.ndarray[nfreq,]
+        Frequency in MHz.
+    tau_cut : float
+        The half width of the stop band in micro-seconds.
+    flag : np.ndarray[nfreq, ntime]
+        Boolean flag that indicates what frequencies are valid
+        as a function of time.
+    epsilon : float
+        The stop-band rejection of the filter.  Defaults to 1e-10.
+
+    Returns
+    -------
+    pinv : np.ndarray[nfreq, nfreq, ntime_uniq]
+        High pass filter for each set of unique frequency flags.
+    index : list of length nuniq_time
+        Maps the last axis of pinv to the original time axis.
+        Apply pinv[:, :, i] to the time samples at index[i].
+    """
 
     ishp = flag.shape
     nfreq = freq.size
     assert ishp[0] == nfreq
-
-    oshp = (nfreq,) + ishp
+    assert len(ishp) == 2
 
     cov = np.eye(nfreq, dtype=np.float64)
     cov += (
-        np.sinc(2.0 * tau_cut * (freq[:, np.newaxis] - freq[np.newaxis, :]) * 1e-3)
-        / epsilon
+        np.sinc(2.0 * tau_cut * (freq[:, np.newaxis] - freq[np.newaxis, :])) / epsilon
     )
 
     uflag, uindex = np.unique(flag.reshape(nfreq, -1), return_inverse=True, axis=-1)
@@ -316,13 +489,37 @@ def highpass_delay_filter(freq, tau_cut, flag, epsilon=1e-8):
     ucov = uflag * cov[np.newaxis, :, :]
 
     pinv = np.linalg.pinv(ucov, hermitian=True) * uflag
-    pinv = np.swapaxes(pinv, 0, 2)[:, :, uindex].reshape(oshp)
+    pinv = np.swapaxes(pinv, 0, 2)
 
-    return pinv
+    index = [np.flatnonzero(uindex == ii) for ii in range(pinv.shape[-1])]
+
+    return pinv, index
 
 
-def bandpass_mmode_filter(ra, m_center, m_cut, flag, epsilon=1e-8):
+def bandpass_mmode_filter(ra, m_center, m_cut, flag, epsilon=1e-10):
+    """Construct a bandpass m-mode filter.
 
+    The pass band will range from [m_center - m_cut, m_center + m_cut].
+
+    Parameters
+    ----------
+    ra : np.ndarray[nra,]
+        Righ ascension in radians.
+    m_center : float
+        The center of the pass band.
+    m_cut : float
+        The half width of the pass band.
+    flag : np.ndarray[nfreq, nra]
+        Boolean flag that indicates what right ascensions are valid
+        as a function of frequency.
+    epsilon : float
+        The stop-band rejection of the filter.  Defaults to 1e-10.
+
+    Returns
+    -------
+    pinv : np.ndarray[nfreq, nra, nra]
+        Bandpass m-mode filter for each frequency.
+    """
     ishp = flag.shape
     nra = ra.size
     assert ishp[-1] == nra
@@ -355,8 +552,28 @@ def bandpass_mmode_filter(ra, m_center, m_cut, flag, epsilon=1e-8):
     return pinv
 
 
-def lowpass_mmode_filter(ra, m_cut, flag, epsilon=1e-8):
+def lowpass_mmode_filter(ra, m_cut, flag, epsilon=1e-10):
+    """Construct a low-pass m-mode filter.
 
+    The pass band will range from [-m_cut, m_cut].
+
+    Parameters
+    ----------
+    ra : np.ndarray[nra,]
+        Righ ascension in radians.
+    m_cut : float
+        The half width of the pass band.
+    flag : np.ndarray[nfreq, nra]
+        Boolean flag that indicates what right ascensions are valid
+        as a function of frequency.
+    epsilon : float
+        The stop-band rejection of the filter.  Defaults to 1e-10.
+
+    Returns
+    -------
+    pinv : np.ndarray[nfreq, nra, nra]
+        Low-pass m-mode filter for each frequency.
+    """
     ishp = flag.shape
     nra = ra.size
     assert ishp[-1] == nra
@@ -383,8 +600,28 @@ def lowpass_mmode_filter(ra, m_cut, flag, epsilon=1e-8):
     return pinv
 
 
-def highpass_mmode_filter(ra, m_cut, flag, epsilon=1e-8):
+def highpass_mmode_filter(ra, m_cut, flag, epsilon=1e-10):
+    """Construct a high-pass m-mode filter.
 
+    The stop band will range from [-m_cut, m_cut].
+
+    Parameters
+    ----------
+    ra : np.ndarray[nra,]
+        Righ ascension in radians.
+    m_cut : float
+        The half width of the stop band.
+    flag : np.ndarray[nfreq, nra]
+        Boolean flag that indicates what right ascensions are valid
+        as a function of frequency.
+    epsilon : float
+        The stop-band rejection of the filter.  Defaults to 1e-10.
+
+    Returns
+    -------
+    pinv : np.ndarray[nfreq, nra, nra]
+        High-pass m-mode filter for each frequency.
+    """
     ishp = flag.shape
     nra = ra.size
     assert ishp[-1] == nra
@@ -409,6 +646,27 @@ def highpass_mmode_filter(ra, m_cut, flag, epsilon=1e-8):
 
 
 def instantaneous_m(ha, lat, dec, u, v, w=0.0):
+    """Calculate the instantaneous fringe-rate.
+
+    Parameters
+    ----------
+    ha : float
+        Hour angle in radians.
+    dec : float
+        Declination in radians.
+    u : float
+        EW baseline distance in wavelengths.
+    v : float
+        NS baseline distance in wavelengths.
+    w : float
+        Vertical baseline distance in wavelengths.
+
+    Returns
+    -------
+    m : float
+        The fringe-rate of the requested location on the sky
+        as measured by the requested baseline.
+    """
 
     deriv = u * (-1 * np.cos(dec) * np.cos(ha))
     deriv += v * (np.sin(lat) * np.cos(dec) * np.sin(ha))
