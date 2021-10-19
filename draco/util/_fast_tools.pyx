3d8da8f775dcb36d263534de64571f28c997b3aa
Tristan Pinsonneault-Marotte <tristpinsm@gmail.com>
2021-10-18 18:20:14 -0700

    refactor(util.tools.invert_no_zero): Calculate inverse inplace.

--- draco/util/_fast_tools.pyx
+++ draco/util/_fast_tools.pyx
@@ -251,3 +251,36 @@ cpdef beamform(float complex [:, :, ::1] vis,
 
     return np.asarray(formed_beam)
 
+
+cdef extern from "float.h" nogil:
+    double DBL_MAX
+    double FLT_MAX
+
+ctypedef fused real_or_complex:
+    double
+    double complex
+    float
+    float complex
+
+@cython.wraparound(False)
+@cython.boundscheck(False)
+cpdef _invert_no_zero(real_or_complex [:] array, real_or_complex [:] out):
+
+    cdef bint cond
+    cdef Py_ssize_t i = 0
+    cdef Py_ssize_t n = array.shape[0]
+    cdef double thresh
+    if (real_or_complex is cython.doublecomplex) or (real_or_complex is cython.double):
+        thresh = 1. / DBL_MAX
+    else:
+        thresh = 1. / FLT_MAX
+
+    if (real_or_complex is cython.doublecomplex) or (real_or_complex is cython.floatcomplex):
+        for i in prange(n, nogil=True):
+            cond = (array[i].real < thresh) if (array[i].real >= 0.) else (-array[i].real < thresh)
+            cond = cond and ((array[i].imag < thresh) if (array[i].imag >= 0.) else (-array[i].imag < thresh))
+            out[i] = 0. if cond else 1. / array[i]
+    else:
+        for i in prange(n, nogil=True):
+            cond = (array[i] < thresh) if (array[i] >= 0.) else (-array[i] < thresh)
+            out[i] = 0. if cond else 1. / array[i]
