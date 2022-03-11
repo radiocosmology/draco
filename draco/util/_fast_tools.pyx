db6d9ca23febe94b80f827388919b6f048ed9207
Richard Shaw <richard@phas.ubc.ca>
2022-03-11 13:08:50 -0800

    perf: optimize cython code generation

--- draco/util/_fast_tools.pyx
+++ draco/util/_fast_tools.pyx
@@ -9,6 +9,7 @@ cimport numpy as np
 from libc.stdint cimport int16_t, int32_t
 from libc.math cimport sin
 from libc.math cimport cos
+from libc.math cimport fabs
 
 cdef inline int int_max(int a, int b) nogil: return a if a >= b else b
 
@@ -264,23 +265,23 @@ ctypedef fused real_or_complex:
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
+@cython.cdivision(True)
 cpdef _invert_no_zero(real_or_complex [:] array, real_or_complex [:] out):
 
     cdef bint cond
     cdef Py_ssize_t i = 0
     cdef Py_ssize_t n = array.shape[0]
-    cdef double thresh
+    cdef double thresh, ar, ai
     if (real_or_complex is cython.doublecomplex) or (real_or_complex is cython.double):
-        thresh = 1. / DBL_MAX
+        thresh = 1.0 / DBL_MAX
     else:
-        thresh = 1. / FLT_MAX
+        thresh = 1.0 / FLT_MAX
 
     if (real_or_complex is cython.doublecomplex) or (real_or_complex is cython.floatcomplex):
         for i in prange(n, nogil=True):
-            cond = (array[i].real < thresh) if (array[i].real >= 0.) else (-array[i].real < thresh)
-            cond = cond and ((array[i].imag < thresh) if (array[i].imag >= 0.) else (-array[i].imag < thresh))
-            out[i] = 0. if cond else 1. / array[i]
+            cond = (fabs(array[i].real) < thresh) and (fabs(array[i].imag) < thresh)
+            out[i] = 0.0 if cond else 1.0 / array[i]
     else:
         for i in prange(n, nogil=True):
-            cond = (array[i] < thresh) if (array[i] >= 0.) else (-array[i] < thresh)
-            out[i] = 0. if cond else 1. / array[i]
+            cond = fabs(array[i]) < thresh
+            out[i] = 0.0 if cond else 1.0 / array[i]
