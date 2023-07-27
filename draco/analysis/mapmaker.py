d212fa43da4a97bb63f563e8e25cc75323cbea6e
Carolin Hofer <cahofer@cedar1.cedar.computecanada.ca>
2023-07-27 11:48:00 -0700

    feat(mapmaker): multi-frequency map-making

--- draco/analysis/mapmaker.py
+++ draco/analysis/mapmaker.py
@@ -14,9 +14,15 @@ class BaseMapMaker(task.SingleTask):
     ----------
     nside : int
         Resolution of output Healpix map.
+    multifreq : bool
+        If this should be a multi-frequency map or not (default: False).
+    nmaps : int
+        How many multi-frequency maps to output.
     """
 
     nside = config.Property(proptype=int, default=256)
+    multifreq = config.Property(proptype=bool, default=False)
+    nmaps = config.Property(proptype=int, default=4)
 
     bt_cache = None
 
@@ -54,7 +60,8 @@ class BaseMapMaker(task.SingleTask):
 
         # Figure out mapping between the frequencies
         bt_freq = self.beamtransfer.telescope.frequencies
-        mm_freq = mmodes.index_map["freq"]["centre"]
+        # mm_freq might need be an attribute of class so BTM can be scaled in loop
+        mm_freq = mmodes.index_map["freq"]["centre"][:]
 
         freq_ind = tools.find_keys(bt_freq, mm_freq, require_match=True)
 
@@ -66,9 +73,17 @@ class BaseMapMaker(task.SingleTask):
         m_weight = mmodes.weight[: (mmax + 1)]
         m_weight = m_weight.redistribute(axis=0)
 
+        # Frequency axis is the number of maps for a multi-frequency map or the number of total frequencies
+        nfreq2 = self.nmaps if self.multifreq else nfreq
+
+        if self.multifreq:
+            freqmap = np.zeros(self.nmaps, dtype=[("centre", float), ("width", float)])
+        else:
+            freqmap = mmodes.index_map["freq"][:]
+
         # Create array to store alms in.
         alm = mpiarray.MPIArray(
-            (nfreq, 4, lmax + 1, mmax + 1),
+            (nfreq2, 4, lmax + 1, mmax + 1),
             axis=3,
             dtype=np.complex128,
             comm=mmodes.comm,
@@ -86,12 +101,21 @@ class BaseMapMaker(task.SingleTask):
             #     self.bt_cache = (m, bt.beam_m(m))
             #     self.log.debug("Cached beamtransfer for m=%i", m)
 
-            for fi in range(nfreq):
-                v = m_array[mi, :, fi].view(np.ndarray)
-                a = alm[fi, ..., mi].view(np.ndarray)
-                Ni = m_weight[mi, :, fi].view(np.ndarray)
+            # Multi-frequency iterator
+            j = int(nfreq / self.nmaps)
 
-                a[:] = self._solve_m(m, freq_ind[fi], v, Ni)
+            for fi in range(nfreq2):
+                fj = slice(fi * j, (fi + 1) * j) if self.multifreq else fi
+
+                # If multi-frequency map, create a frequency map
+                if self.multifreq:
+                    freqmap["centre"][fi] = np.mean(mm_freq[fj])
+                    freqmap["width"][fi] = abs(mm_freq[fj][0] - mm_freq[fj][-1])
+
+                v = m_array.local_array[mi, :, fj]
+                a = alm.local_array[fi, ..., mi]
+                Ni = m_weight.local_array[mi, :, fj]
+                a[:] = self._solve_m(m, freq_ind[fj], v, Ni)
 
         self.bt_cache = None
 
@@ -100,7 +124,7 @@ class BaseMapMaker(task.SingleTask):
 
         # Copy into square alm array for transform
         almt = mpiarray.MPIArray(
-            (nfreq, 4, lmax + 1, lmax + 1),
+            (nfreq2, 4, lmax + 1, lmax + 1),
             dtype=np.complex128,
             axis=0,
             comm=mmodes.comm,
@@ -112,7 +136,9 @@ class BaseMapMaker(task.SingleTask):
         maps = hputil.sphtrans_inv_sky(alm, self.nside)
         maps = mpiarray.MPIArray.wrap(maps, axis=0)
 
-        m = containers.Map(nside=self.nside, axes_from=mmodes, comm=mmodes.comm)
+        m = containers.Map(
+            nside=self.nside, freq=freqmap, axes_from=mmodes, comm=mmodes.comm
+        )
         m.map[:] = maps
 
         return m
@@ -164,6 +190,49 @@ class DirtyMapMaker(BaseMapMaker):
 
         # Solve for the dirty map alms
         a = np.dot(bm.T.conj(), Ni * v)
+
+        # Reshape to the correct output
+        a = a.reshape(bt.telescope.num_pol_sky, bt.telescope.lmax + 1)
+
+        return a
+
+
+class DirtyMapMakerMultiFreq(BaseMapMaker):
+    r"""Generate a dirty map.
+
+    Notes
+    -----
+
+    The dirty map is produced by generating a set of :math:`a_{lm}` coefficients
+    using
+
+    .. math:: \hat{\mathbf{a}} = \mathbf{B}^\dagger \mathbf{N}^{-1} \mathbf{v}
+
+    and then performing the spherical harmonic transform to get the sky intensity.
+    """
+
+    spectral_mean = config.Property(proptype=float, default=-2.7)
+
+    def _solve_m(self, m, f, v, Ni):
+        bt = self.beamtransfer
+        bt_freq = bt.telescope.frequencies
+        a_dirty = np.zeros((len(f), bt.nsky), dtype=np.complex128)
+
+        f0 = np.mean(bt_freq[f])
+
+        # Massage the arrays into shape
+        for i in range(len(f)):
+            self.log.debug("Processing frequency" + str(bt_freq[f[i]]))
+            vi = v[:, i].reshape(bt.ntel)
+            Nii = Ni[:, i].reshape(bt.ntel)
+            # BTM needs to be scaled to mean frequency (\nu_0 / nu_i)^{\spectra_mean}
+            bm = bt.beam_m(m, fi=f[i]).reshape(bt.ntel, bt.nsky) * (
+                f0 / bt_freq[f[i]]
+            ) ** (self.spectral_mean)
+            a_dirty[i] = np.dot(bm.T.conj(), Nii * vi)
+
+        # Solve for the dirty map alms
+        a = np.sum(a_dirty, axis=0)
 
         # Reshape to the correct output
         a = a.reshape(bt.telescope.num_pol_sky, bt.telescope.lmax + 1)
@@ -281,6 +350,75 @@ class WienerMapMaker(BaseMapMaker):
             pCi = np.identity(bt.ntel) + np.dot(bmt * S_diag[np.newaxis, :], bth)
             v_int = la.solve(pCi, vt, sym_pos=True)
             a_wiener = S_diag * np.dot(bth, v_int)
+
+        # Copy the solution into a correctly shaped array output
+        a = np.zeros((bt.telescope.num_pol_sky, bt.telescope.lmax + 1), dtype=v.dtype)
+        a[:, m:] = a_wiener.reshape(bt.telescope.num_pol_sky, -1)
+
+        return a
+
+
+class WienerMapMakerMultiFreq(BaseMapMaker):
+    r"""Generate a multifrequency Wiener Map"""
+
+    prior_amp = config.Property(proptype=float, default=1.0)
+    prior_tilt = config.Property(proptype=float, default=0.5)
+    spectral_mean = config.Property(proptype=float, default=-2.7)
+
+    bt_cache = None
+
+    def _solve_m(self, m, f, v, Ni):
+        import scipy.linalg as la
+
+        bt = self.beamtransfer
+        bt_freq = bt.telescope.frequencies
+
+        f0 = np.mean(bt_freq[f])
+
+        # Construct the signal covariance matrix
+        l = np.arange(bt.telescope.lmax + 1)
+        l[0] = 1  # Change l=0 to get around singularity
+        l = l[m:]  # Trim off any l < m
+        cl_TT = self.prior_amp**2 * l ** (-self.prior_tilt)
+        S_diag = np.concatenate([cl_TT] * 4)
+        s = S_diag.shape[0]
+
+        # Allocate an array for dirty map and covariance
+        a_dirty = np.zeros(s, dtype=np.complex128)
+        Ci = np.zeros((s, s), dtype=np.complex128)
+
+        # Massage the arrays into shape
+        for i in range(len(f)):
+            self.log.debug("Processing frequency" + str(bt_freq[f[i]]))
+            # Get transfer for this m and f
+            if self.bt_cache is not None and self.bt_cache[0] == m:
+                bm = self.bt_cache[1][f[i]]
+            else:
+                bm = bt.beam_m(m, fi=f[i])
+            # BTM needs to be scaled by frequency (\nu_0 / nu_i)^{spectral_mean}
+            bm = bm[..., m:].reshape(bt.ntel, -1) * (f0 / bt_freq[f[i]]) ** (
+                self.spectral_mean
+            )
+
+            # Massage the arrays into shape
+            vi = v[:, i].reshape(bt.ntel)
+            Nii = Ni[:, i].reshape(bt.ntel)
+            Nh = Nii**0.5
+
+            # Construct pre-whitened beam and beam-conjugated matrices
+            bmt = bm * Nh[:, np.newaxis]
+            bth = bmt.T.conj()
+
+            # Pre-whiten the visibilities
+            vt = Nh * vi
+
+            # For bt.ntel > bt.nsky
+            Ci += np.diag(1.0 / S_diag) + np.dot(bth, bmt)
+            a_dirty += np.dot(bth, vt)  # Find the dirty map
+            # TO DO: Is there a better way to rearrange for bt.ntel > bt.nsky?
+
+        # Solve to find the multi-frequency a
+        a_wiener = la.solve(Ci, a_dirty, sym_pos=True)
 
         # Copy the solution into a correctly shaped array output
         a = np.zeros((bt.telescope.num_pol_sky, bt.telescope.lmax + 1), dtype=v.dtype)
