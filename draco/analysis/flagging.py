8babef268d55b705f3028452b152ae050b273932
Liam Gray <lgray@phas.ubc.ca>
2024-12-09 17:55:37 -0700

    feat(flagging): Add broadcasting to ApplyGenericMask and alias for compatibility

--- draco/analysis/flagging.py
+++ draco/analysis/flagging.py
@@ -451,43 +451,6 @@ class MaskBadGains(task.SingleTask):
         return mask_cont
 
 
-class MaskBeamformedOutliers(task.SingleTask):
-    """Mask beamformed visibilities that deviate from our expectation for noise.
-
-    This is operating under the assumption that, after proper foreground filtering,
-    the beamformed visibilities should be consistent with noise.
-    """
-
-    def process(self, data, mask):
-        """Mask outlier beamformed visibilities.
-
-        Parameters
-        ----------
-        data : FormedBeam, FormedBeamHA, or RingMap
-            Beamformed visibilities.
-
-        mask : FormedBeamMask, FormedBeamHAMask, or RingMapMask
-            Container with a boolean mask where True indicates
-            a beamformed visibility that should be ignored.
-
-        Returns
-        -------
-        data : FormedBeam or RingMap
-            The input container with the weight dataset set to zero
-            for samples that were identified as outliers.
-        """
-        # Redistribute data over frequency
-        data.redistribute("freq")
-        mask.redistribute("freq")
-
-        # Multiply the weights by the inverse of the mask
-        flag = ~mask.mask[:].local_array
-
-        data.weight[:].local_array[:] *= flag.astype(np.float32)
-
-        return data
-
-
 class MaskBeamformedWeights(task.SingleTask):
     """Mask beamformed visibilities with anomalously large weights before stacking.
 
@@ -2276,6 +2239,64 @@ class ApplyTimeFreqMask(task.SingleTask):
         tsc.weight[:].local_array[:] *= (~mask[bcast_slice]).astype(np.float32)
 
         return tsc
+
+
+class ApplyGenericMask(task.SingleTask):
+    """Apply a mask to a dataset with arbitrary axes.
+
+    All of the mask axes must be present in the dataset, but
+    the dataset can have additional axes.
+
+    Assumes that a sample marked `True` in the mask dataset
+    should be flagged.
+    """
+
+    def process(self, data: containers.ContainerBase, mask: containers.ContainerBase):
+        """Apply the mask to the dataset weights.
+
+        Reorder the mask axes and add broadcasting axes if necessary.
+
+        Parameters
+        ----------
+        data
+            Any container with a frequency axis.
+        mask
+            Any container whose axes are a subset of the axes in data
+
+        Returns
+        -------
+        data
+            The input container with the weight dataset set to zero
+            for masked samples.
+        """
+        # Pull out the axes of each dataset
+        daxes = list(data.weight.attrs["axis"])
+        maxes = list(data.mask.attrs["axis"])
+
+        # Make sure all the mask axes exist in the data
+        if any(ax not in daxes for ax in maxes):
+            raise NameError("At least ones axis in `mask` does not exist in `data`.")
+
+        # Redistribute over frequency, assuming that all containers have
+        # a frequency axis
+        data.redistribute("freq")
+        mask.redistribute("freq")
+
+        # Rearrange the existing mask axes to match their order in the dataset
+        tinds = tuple(maxes.index(ax) for ax in daxes if ax in maxes)
+        mask = mask.mask[:].local_array.transpose(tinds)
+
+        # Add broadcasting axes now that mask axes are in the correct order
+        bcast_slobj = tuple(slice(None) if ax in maxes else np.newaxis for ax in daxes)
+
+        # Multiply the inverse mask into the weights
+        data.weight[:].local_array[:] *= (~mask[bcast_slobj]).astype(data.weight.dtype)
+
+        return data
+
+
+# Alias for backwards compatibility
+MaskBeamformedOutliers = ApplyGenericMask
 
 
 class ApplyBaselineMask(task.SingleTask):
