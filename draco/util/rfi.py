8c4c643ca85fe2e2d23fc68d96390a29dd4f6ba9
Liam Gray <lgray@phas.ubc.ca>
2025-09-08 16:34:58 -0700

    perf(rfi): fast scale-invariant rank operation

--- draco/util/rfi.py
+++ draco/util/rfi.py
@@ -1,6 +1,7 @@
 """Collection of routines for RFI excision."""
 
 import numpy as np
+import numpy.typing as npt
 from scipy.ndimage import correlate1d
 
 
@@ -143,43 +144,125 @@ def sumthreshold_py(
 sumthreshold = sumthreshold_py
 
 
-# Scale-invariant rank (SIR) functions
-def sir1d(basemask, eta=0.2):
+def _sir_lastaxis(
+    basemask: npt.NDArray[np.bool_], eta: float = 0.2, axis=-1
+) -> npt.NDArray[np.bool]:
     """Numpy implementation of the scale-invariant rank (SIR) operator.
 
     For more information, see arXiv:1201.3364v2.
 
     Parameters
     ----------
-    basemask : numpy 1D array of boolean type
+    basemask
         Array with the threshold mask previously generated.
         1 (True) for flagged points, 0 (False) otherwise.
-    eta : float
+    eta
         Aggressiveness of the method: with eta=0, no additional samples are
         flagged and the function returns basemask. With eta=1, all samples
         will be flagged. The authors in arXiv:1201.3364v2 seem to be convinced
         that 0.2 is a mostly universally optimal value, but no optimization
         has been done on CHIME data.
+    axis
+        Axis along which to apply the SIR operator. Default is -1,
+        which applies it along the last axis.
 
     Returns
     -------
-    mask : numpy 1D array of boolean type
+    mask
         The mask after the application of the (SIR) operator. Same shape and
         type as basemask.
     """
-    n = basemask.size
-    psi = basemask.astype(np.float64) - 1.0 + eta
+    # Move the filtetr axis to the end and copy to ensure contiguous array
+    basemask = np.moveaxis(basemask, axis, -1).copy()
 
-    M = np.zeros(n + 1, dtype=np.float64)
-    M[1:] = np.cumsum(psi)
+    M = np.zeros((*basemask.shape[:-1], basemask.shape[-1] + 1), dtype=np.float64)
+    # Copy the basemask into the appropriate slice of M,
+    # which will automatically cast to float64. Use `M` to
+    # avoid an additional allocation
+    M[..., 1:] = basemask
+    M[..., 1:] += eta - 1.0
 
-    MP = np.minimum.accumulate(M)[:-1]
-    MQ = np.concatenate((np.maximum.accumulate(M[-2::-1])[-2::-1], M[-1, np.newaxis]))
+    # cumulative sum stored directly into `M` to avoid
+    # an additional allocation
+    # NOTE: neither `cumsum` not `accumulate` seem to use
+    # multiple threads, so this seems like a fairly easy place
+    # for significant performance improvement
+    np.cumsum(M[..., 1:], axis=-1, out=M[..., 1:])
 
-    return (MQ - MP) >= 0.0
+    MP = np.minimum.accumulate(M, axis=-1)[..., :-1]
+    # Re-use `M` again
+    np.maximum.accumulate(M[..., -2::-1], axis=-1, out=M[..., -2::-1])
+
+    basemask |= M[..., 1:] >= MP
+
+    return np.moveaxis(basemask, -1, axis)
 
 
-def sir(basemask, eta=0.2, only_freq=False, only_time=False):
+# Alias for backward compatibility
+sir1d = _sir_lastaxis
+
+
+def scale_invariant_rank(
+    basemask: npt.NDArray[np.bool_],
+    eta: float | tuple[float, ...] = 0.2,
+    axis: int | tuple[int, ...] = -1,
+) -> npt.NDArray[np.bool]:
+    """Apply the scale-invariant rank (SIR) operator along one or more axes.
+
+    This is a wrapper for `sir1d`.  It loops over the provided axes, applying
+    `sir1d` along each axis in turn.  It returns the logical OR of these masks.
+
+    Parameters
+    ----------
+    basemask
+        The previously generated threshold mask.
+        1 (True) for masked points, 0 (False) otherwise.
+    eta
+        Aggressiveness of the method: with eta=0, no additional samples are
+        flagged and the function returns basemask. With eta=1, all samples
+        will be flagged. If a tuple is provided, it must have the same length
+        as `axis`, and the corresponding value will be used for each axis.
+    axis
+        Axis or axes along which to apply the SIR operator. Default is -1,
+        which applies it along the last axis.
+
+    Returns
+    -------
+    mask
+        The mask after the application of the SIR operator.
+    """
+    if basemask.ndim < 1:
+        raise ValueError("basemask must have at least one dimension.")
+
+    if isinstance(axis, int):
+        axis = (axis,)
+
+    if isinstance(eta, float | int):
+        eta = (eta,) * len(axis)
+
+    if len(eta) != len(axis):
+        raise ValueError(
+            "If eta is a tuple, it must have the same length as axis."
+            f"Got len(eta)={len(eta)} and len(axis)={len(axis)}."
+        )
+
+    # Avoids an unnecessary copy in the case where only one axis is requested
+    newmask = _sir_lastaxis(basemask, eta=eta[0], axis=axis[0])
+
+    # Iterate over the axes, applying sir1d to the
+    # base mask along each axis
+    for ax, et in zip(axis[1:], eta[1:]):
+        newmask |= _sir_lastaxis(basemask, eta=et, axis=ax)
+
+    return newmask
+
+
+def sir(
+    basemask: npt.NDArray[np.bool_],
+    eta: float = 0.2,
+    only_freq: bool = False,
+    only_time: bool = False,
+) -> npt.NDArray[np.bool]:
     """Apply the SIR operator over the frequency and time axes for each product.
 
     This is a wrapper for `sir1d`.  It loops over times, applying `sir1d`
@@ -188,23 +271,37 @@ def sir(basemask, eta=0.2, only_freq=False, only_time=False):
 
     Parameters
     ----------
-    basemask : np.ndarray[nfreq, nprod, ntime] of boolean type
+    basemask
         The previously generated threshold mask.
         1 (True) for masked points, 0 (False) otherwise.
-    eta : float
+    eta
         Aggressiveness of the method: with eta=0, no additional samples are
         flagged and the function returns basemask. With eta=1, all samples
         will be flagged.
-    only_freq : bool
+    only_freq
         Only apply the SIR operator across the frequency axis.
-    only_time : bool
+    only_time
         Only apply the SIR operator across the time axis.
 
     Returns
     -------
-    mask : np.ndarray[nfreq, nprod, ntime] of boolean type
+    mask
         The mask after the application of the SIR operator.
     """
+    import warnings
+
+    warnings.warn(
+        "The sir function is deprecated and will be removed in a future release. "
+        "Please use the updated `scale_invariant_rank` function with `axis=(0, -1)`.",
+        DeprecationWarning,
+    )
+
+    if basemask.ndim != 3:
+        raise ValueError(
+            "basemask must be a 3D array with [freq, prod, time] axes. "
+            f"Got {basemask.ndim}D array instead."
+        )
+
     if only_freq and only_time:
         raise ValueError("Only one of only_freq and only_time can be True.")
 
@@ -215,10 +312,10 @@ def sir(basemask, eta=0.2, only_freq=False, only_time=False):
     for pp in range(nprod):
         if not only_time:
             for tt in range(ntime):
-                newmask[:, pp, tt] |= sir1d(basemask[:, pp, tt], eta=eta)
+                newmask[:, pp, tt] |= _sir_lastaxis(basemask[:, pp, tt], eta=eta)
 
         if not only_freq:
             for ff in range(nfreq):
-                newmask[ff, pp, :] |= sir1d(basemask[ff, pp, :], eta=eta)
+                newmask[ff, pp, :] |= _sir_lastaxis(basemask[ff, pp, :], eta=eta)
 
     return newmask
