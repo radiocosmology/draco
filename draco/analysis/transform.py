2798e4099ecb11e3abcff180b466801544b3a877
Liam Gray <lgray@phas.ubc.ca>
2025-09-15 10:05:18 -0700

    feat(transform): extend ReduceBase with a hook for custom weights

--- draco/analysis/transform.py
+++ draco/analysis/transform.py
@@ -1977,19 +1977,19 @@ class ReduceBase(task.SingleTask):
         out.redistribute(new_ax_name)
 
         # Get the weights
-        if hasattr(data, "weight"):
+        weight, w_axes = self._get_weights(data)
+
+        if weight is not None:
             # The weights should be distributed over the same axis as the array,
             # even if they don't share all the same axes
-            w_axes = list(data.weight.attrs["axis"])
             new_weight_ax = w_axes.index(new_ax_name)
-            weight = data.weight[:].redistribute(new_weight_ax)
+            weight = weight.redistribute(new_weight_ax)
             # Insert a size 1 axis for each missing axis in the weights
             wslc = [slice(None) if ax in w_axes else None for ax in ds_axes]
             weight = weight.local_array[tuple(wslc)]
         else:
-            self.log.info("No weights available. Using equal weighting.")
+            weight = np.ones_like(ds[:].local_array, dtype=np.float32)
             wslc = None
-            weight = np.ones(ds.local_shape, ds.dtype)
 
         # Apply the reduction, ensuring that the weights have the correct dimension
         weight = np.broadcast_to(weight, ds.local_shape, subok=False)
@@ -2014,6 +2014,18 @@ class ReduceBase(task.SingleTask):
         out.redistribute(ds_axes[original_ax_id])
 
         return out
+
+    def _get_weights(self, data: containers.ContainerBase) -> mpiarray.MPIArray | None:
+        """Get the weights to use for the reduction."""
+        if not hasattr(data, "weight") and self.weighting != "none":
+            raise RuntimeError(
+                "No weights available. Cannot use weighted or masked weighting."
+            )
+
+        if hasattr(data, "weight"):
+            return data.weight[:], list(data.weight.attrs["axis"])
+
+        return None
 
     def _make_output_container(
         self, data: containers.ContainerBase
