b9c4e6528c9a5fd3d4f4da561d980d4ac61aa1e2
Liam Gray <lgray@phas.ubc.ca>
2025-09-17 15:55:03 -0700

    feat(flagging): add polarisation masking option to MaskBaselines

--- draco/analysis/flagging.py
+++ draco/analysis/flagging.py
@@ -192,6 +192,9 @@ class MaskBaselines(tasklib.base.ContainerTask):
     mask_short_ns : float, optional
         Mask out baselines shorter then a given distance in the North-South
         direction.
+    mask_pol : list of str, optional
+        List of polarisation products to mask. Each entry should be a string
+        of length 2, e.g. ["XX", "YY"].
     missing_threshold : float, optional
         Mask any baseline that is missing more than this fraction of samples. This is
         measured relative to other baselines.
@@ -213,6 +216,8 @@ class MaskBaselines(tasklib.base.ContainerTask):
     mask_short = config.Property(proptype=float, default=None)
     mask_short_ew = config.Property(proptype=float, default=None)
     mask_short_ns = config.Property(proptype=float, default=None)
+
+    mask_pol = config.Property(proptype=list, default=None)
 
     weight_threshold = config.Property(proptype=float, default=None)
     missing_threshold = config.Property(proptype=float, default=None)
@@ -305,6 +310,15 @@ class MaskBaselines(tasklib.base.ContainerTask):
                 > self.missing_threshold,
                 out=mask,
             )
+
+        if self.mask_pol is not None:
+            pols = np.char.array(self.telescope.polarisation)[
+                self.telescope.uniquepairs
+            ]
+            pols = pols[:, 0] + pols[:, 1]
+
+            for p in self.mask_pol:
+                combine_func(mask, (pols == p)[np.newaxis, :, np.newaxis], out=mask)
 
         if self.share == "all":
             ssc = ss
